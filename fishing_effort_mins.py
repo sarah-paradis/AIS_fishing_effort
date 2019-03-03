6596dd4acfb2868946823ddcce602fa6d3213bff
Sarah Paradis <sarah.paradisvilar@gmail.com>
2019-03-03 18:29:30 +0100

    Saving long dataframes into several excel files.

--- fishing_effort_mins.py
+++ fishing_effort_mins.py
@@ -8,13 +8,13 @@ Created on Wed Feb 13 16:14:13 2019
 
 import pandas as pd
 import os
-
+import io
 import timeit
 from contextlib import contextmanager
 
 @contextmanager
-def print_with_time(s):
-    print(s, end="", flush=True)
+def print_with_time(*s):
+    print(*s, end="", flush=True)
     start = timeit.default_timer()
     yield
     print("\t[%.2fs]" %(timeit.default_timer() - start), flush=True)
@@ -54,27 +54,50 @@ def fishing_effort_min(file_dir):
                               'Longitud_decimal', 'Sog', 'Cog', 'Heading', 'Rot',
                               'Eslora', 'Manga']]
     # Create a new column with only the month and year (to make a .csv file with this data)
-    with print_with_time('Grouping data by month'):
+    with print_with_time('Grouping data by month and day'):
         df_min['Mes'] = df_min['Fecha_Posicion_min'].dt.to_period('M')
+        df_min['Fecha'] = df_min['Fecha_Posicion_min'].dt.to_period('D')
 
     # Extract the months in the files to later create individual dataframes 
     # and the names of the output .csv files for each month
-        months = list(df_min.Mes.unique()) # list of the months from which the DataFrames will be extracted
-        months_name = [month.strftime('%m_%Y') for month in months] # list of the names of the months for the .csv files
+        months = sorted(list(df_min.Mes.unique())) # list of the months from which the DataFrames will be extracted
+        months_name = [month.strftime('%Y_%m') for month in months] # list of the names of the months for the .csv files
+        days = []
+        for month in months:
+            day_month = sorted(list(df_min['Fecha'][df_min['Mes'] == month].unique()))
+            days.append(day_month)
     
-    for month, month_name in zip(months, months_name):
+    # Exporting files
+    max_excel_rows = 65000 # actually it is 65536 (2^16)
+    for month, month_name, idx in zip(months, months_name, range(len(months))):
         df_month = df_min[df_min['Mes'] == month] # Create a new DataFrame with the data of that specific month
-        file_name = month_name+'.csv'
-        print('Exporting '+file_name+' to .csv file')
-        df_month.to_csv(os.path.join(dir_output,file_name), index=None)
-            
+        file_name = month_name+'.xls'
+        if len(df_month) < max_excel_rows:
+            with print_with_time('Exporting '+file_name+' to .xls file'):
+                df_month.to_excel(os.path.join(dir_output,file_name), index=None)
+        else:
+            with print_with_time('Exporting '+ file_name + ' in different .xls files'):
+                while len(days[idx]) > 0: # save files
+                    for i in range(len(days[idx])): # days
+                        if len(df_month[df_month['Fecha'].isin(days[idx][:i+1])]) > max_excel_rows:
+                            i = i-1
+                            break   
+                    file_name = month_name+'_'+days[idx][i].strftime('%d')+'.xls'
+                    file_name = os.path.join(dir_output,file_name)
+                    df_save = df_month[df_month['Fecha'].isin(days[idx][:i+1])]
+                    assert len(df_save) > 0, "Too many rows for day %s. Cannot save in .xls."%days[idx][i+1].strftime('%d')
+                    df_save.to_excel(file_name, index=None)
+                    days[idx] = days[idx][i+1:]                
+    
   
 if __name__ == "__main__": 
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
+    import argparse
+    parser = argparse.ArgumentParser()
     parser.add_argument("file_dir",
                         type=str,
                         help="Directory where all AIS data is saved as Excel files (.xls)")
+    
     args = parser.parse_args()
+#    args = argparse.Namespace(file_dir='DATA') 'Data' added in the Spyder Run->Configuration per file->command line options
     
     fishing_effort_min(args.file_dir)
